ca0f7faaf464e6f90b7c15dc46cc8d7dbe79e153
SiddiqFitriansyah <siddiqfitriansyah14@gmail.com>
2023-12-07 04:18:10 +0700

    Create function FormInputSiswa

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -93,3 +93,14 @@ fun EntrySiswaBody(
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {},
+    enabled: Boolean = true
+){
+
+}

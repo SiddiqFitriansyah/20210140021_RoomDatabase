c16179a0bc630a613b60251620f046497a776e14
SiddiqFitriansyah <siddiqfitriansyah14@gmail.com>
2023-12-07 04:27:05 +0700

    Create outlinedtextfield telpon

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -123,5 +123,13 @@ fun FormInputSiswa(
             enabled = enabled,
             singleLine = true
             )
+        OutlinedTextField(
+            value = detailSiswa.telpon,
+            onValueChange = {onValueChange(detailSiswa.copy(telpon = it)) },
+            label = { Text(stringResource(id = R.string.telpon)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
     }
 }
